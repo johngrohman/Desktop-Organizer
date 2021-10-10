7b7024bf1d6d4b949e5770941176c93525da26e6
John Grohman <grohmanjp@gmail.com>
2021-10-10 01:24:39 -0500

    Updated init.sh

--- init.sh
+++ init.sh
@@ -9,29 +9,21 @@ mkdir ../text/pdfs
 mkdir ../video/
 mkdir ../images/
 mkdir ../other/
-echo "..."
-sleep 0.5s
 mkdir ../other/internet/
 mkdir ../other/compressed/
 mkdir ../other/disc/
 mkdir ../programming/
-echo ".."
-sleep 0.5s
 mkdir ../programming/database/
 mkdir ../other/executabes/
 mkdir ../other/fonts/
 mkdir ../text/presentations/
 mkdir ../programming/candc++/
 mkdir ../programming/java/
-echo "."
-sleep 0.5s
 mkdir ../programming/python/
 mkdir ../programming/shell/
 mkdir ../text/microsoft/excel/
 mkdir ../text/other/
 mkdir ../text/other/system/
-sleep 1s
 clear
 echo "Complete"
-sleep 0.5s
 clear
